5a27a1aa6a26597902e2804745f0f7d522d8253b
kelvinrandu <randukelvin@gmail.com>
2021-06-22 13:19:44 +0300

    added and styled header

--- drive/src/App.js
+++ drive/src/App.js
@@ -1,16 +1,24 @@
-
+import React ,{useState} from 'react';
 import './App.css';
+import Header from './components/header';
 
 function App() {
+  const [user, setUser] = useState({
+    displayName:"kelvin randu",
+    email: "randukelvin@gmail.com",
+    emailVerified: true,
+    phoneNumber:  null,
+    photoURL:"https://lh3.googleusercontent.com/ogw/ADea4I5aNx4aP1I8gm0lDsnn4Mhd3PQwqJZbtK4pjYwh=s32-c-mo"
+  })
   return (
     <div className="App">
-      <header className="App-header">
+      <Header userPhoto={user.photoURL}/>
     
         <p>
           welcome to google drive mtaani
         </p>
 
-      </header>
+    
     </div>
   );
 }
